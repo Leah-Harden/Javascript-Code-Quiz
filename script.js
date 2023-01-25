0e9fafe00a0f2e67e86f4943235ab77011931c70
Your Name <you@example.com>
2023-01-24 20:12:55 -0500

    add event listeners

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 //var
-var secs
+var sec
 
 var questions = [{
     id: 1,
     titles: "How do you call a Function?",
-    answers: ["This is wrong a", "it is wrong b", "is wrong c "],
-    right: 123
-}
-    , {
+    answers: ["This is wrong a", "right", "it is wrong b", "is wrong c "],
+    right: 1
+},
+{
     id: 2,
     titles: "How do you call a Function2?",
-    answers: ["worag2a", "woang2b", "worg2c"],
-    right: 113
+    answers: ["right", "worag2a", "woang2b", "worg2c"],
+    right: 0
 }
-    ,
-{
+    , {
     id: 3,
     titles: "How do you call a Function3?",
-    answers: ["wrogn3a", "wrwaf3b", "wrsdaf3c"],
-    right: 111
-}
-]
-//var list = []
-//list.push(question1, question2, question3);
-//console.log(list)
-
+    answers: ["wrogn3a", "wrwaf3b", "right", "wrsdaf3c"],
+    right: 2
+}]
 //var
 
 
@@ -32,31 +26,35 @@ var questions = [{
 
 function rightQuestion() {
     for (var i = 0; i < questions.length; i++) {
-        document.getElementById("wrong1").innerHTML = questions[i].answers[0]
-        document.getElementById("wrong2").innerHTML = questions[i].answers[1]
-        document.getElementById("wrong3").innerHTML = questions[i].answers[2]
-        document.getElementById("title").innerHTML = questions[i].titles
-        document.getElementById("right").innerHTML = questions[i].right
+        var random = Math.floor(Math.random() * questions[i].answers.length)
+        {
+            if (random === 0) {
+                document.getElementById("choice0").innerHTML = questions[i].right;
+            } else {
+
+                document.getElementById("choice0").innerHTML = questions[i].answers[0]
+                document.getElementById("choice1").innerHTML = questions[i].answers[1]
+                document.getElementById("choice2").innerHTML = questions[i].answers[2]
+                document.getElementById("choice3").innerHTML = questions[i].right
+                document.getElementById("title").innerHTML = questions[i].titles
+            }
+        }
     }
 }
-
 
 
 
 //listerers
 
 document.getElementById("start quiz").addEventListener("click", startGame);
-document.getElementById("right").addEventListener("click", rightQuestion);
+document.getElementById("choice3").addEventListener("click", rightQuestion);
 
 
 //listerers
 
-
-
-
 //timer
 function timer() {
-    document.getElementById("wrong1", "wrong2", "wrong3").addEventListener("click", wrongChoice);
+    //document.getElementById("choice0", "choice1", "choice2").addEventListener("click", wrongChoice);
     var secs = 59;
     var countDown = setInterval(function () {
         document.getElementById("time-number").innerHTML = secs;
@@ -66,16 +64,13 @@ function timer() {
             document.getElementById("time-number").innerHTML = "Done";
         }
     }, 1000)
-    function wrongChoice() {
-        console.log("hello")
+    $("#choice0,#choice1,#choice2").click(wrongChoice(){
+        console.log("hello"),
         secs -= 10
-    }
-
+        })
 }
 
 //timer
-
-
 function startGame() {
     var x = document.getElementById("home-page");
     x.style.display = "none";
@@ -84,4 +79,3 @@ function startGame() {
     timer()
 
 }
-
